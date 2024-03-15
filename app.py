d33818633d710d0557fb918b7d579882c9c3a3f1
Jaylynn <jmiri697@gmail.com>
2024-03-15 10:10:34 -0400

    BZZZZP

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
-from blueprint import damage
+from blueprint import trap
 import random
 # creates and defines an app using flask
 def make_app():
     app = Flask(__name__)
-    app.register_blueprint(damage)
+    app.register_blueprint(trap)
     return app
 # sets up the website
 if __name__ == '__main__':
     app = make_app()
-    app.run(host='localhost', port=8081, debug=True)
+    app.run(host='localhost', port=8083, debug=True)
--- blueprint.py
+++ blueprint.py
@@ -2,6 +2,11 @@ import random
 from flask import Flask, Blueprint
 
 trap = Blueprint('trap', __name__)
-@trap.route('/start', methods=['GET', 'POST'])
-def start():
+@trap.route('/beginning', methods=['GET', 'POST'])
+def trap():
     return 'Do you want to play a game?'
+    return redirect('/tape/recoding')
+
+@trap.route('/tape/recording', methods=['GET', 'POST'])
+def tape_recording():
+    return ''
